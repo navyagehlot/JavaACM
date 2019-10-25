729159c43d2992e968419ab635c0779c5fb3945d
navyagehlot <57015798+navyagehlot@users.noreply.github.com>
2019-10-26 00:22:04 +0530

    Update Program1.java

--- Program1.java
+++ Program1.java
@@ -5,8 +5,8 @@ class Program1
 	public static void main(String args[])throws IOException
 	{
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Gimme a number");
+		System.out.println("Give me a number");
 		int i=sc.nextInt();
-		System.out.println("Bruh the number you gave me was "+i);
+		System.out.println("The number given is "+i);
 	}
 }
