05782196261b556cc7f3fcb3bcf0fabd3fe80aa3
navyagehlot <57015798+navyagehlot@users.noreply.github.com>
2019-10-26 00:22:48 +0530

    Update Program2.java

--- Program2.java
+++ Program2.java
@@ -4,9 +4,9 @@ class Program2
 {
 	public static void main(String args[]) throws IOException
 	{
-		int n=3;
-		double n1=3;
-		System.out.println(n);
-    System.out.println(n1);
+		int k=3;
+		double m=3;
+		System.out.println(k);
+    System.out.println(m);
 	}
 }
